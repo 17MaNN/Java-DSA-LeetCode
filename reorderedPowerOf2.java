5881cf0118f832221f335e41ec20324b98ab9dcf
17MaNN <142076383+17MaNN@users.noreply.github.com>
2025-08-10 23:41:54 +0530

    Create reorderedPowerOf2.java

---
+++ reorderedPowerOf2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean reorderedPowerOf2(int n) {
+        String tar = sortDigits(n);
+
+        for(int i=0 ; i<31; i++){
+            int power= 1 << i;
+            if(tar.equals(sortDigits(power))){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private String sortDigits(int num) {
+        char[] arr = String.valueOf(num).toCharArray();
+        Arrays.sort(arr);
+        return new String(arr);
+    }
+}
